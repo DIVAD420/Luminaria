f6c6a4d02d01453600a9c16523dda3cd4bd5eef3
DIVAD420 <dcam2008@gmail.com>
2023-10-17 22:21:47 +1000

    added survivor detection for the killer

---
+++ NewLuminaria/Assets/Assets1 1/Scripts/SurvivorDetectionScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurvivorDetectionScript : MonoBehaviour
+{
+    public float detectionRadius;
+
+    public LayerMask whatIsKiller;
+
+    public bool isThereKiller = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isThereKiller = Physics.CheckSphere(transform.position, detectionRadius, whatIsKiller);
+
+        if (isThereKiller)
+        {
+            gameObject.layer = LayerMask.NameToLayer("IsDetected");
+        }
+        else
+        {
+            gameObject.layer = LayerMask.NameToLayer("IsNotDetected");
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+
+        Gizmos.DrawWireSphere(transform.position, detectionRadius);
+    }
+}
